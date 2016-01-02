5841999979a244cb92a81cf4249b7ba0a42472dc
Donald Averill <donaldaverill@gmail.com>
2016-01-02 14:18:46 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+[Meteor](https://www.meteor.com) Prototype Package for fourquet.io
+--
+Use as a starter package for package based Meteor apps or for package development. This package is not intended to be installed.
+
+#### To use:
+```bash
+$ git clone https://github.com/fourquet/meteor-package-proto.git packageName
+$ cd packageName
+$ rm -rf .git
+```
---
+++ package.js
@@ -0,0 +1,31 @@
+Package.describe({
+  name: 'fourquet:proto',
+  version: '0.0.1',
+  summary: 'Meteor Prototype Package for fourquet.io',
+  git: 'https://github.com/fourquet/meteor-package-proto.git',
+  documentation: 'README.md',
+  license: 'LICENSE',
+});
+
+const packages = [
+  'ecmascript',
+];
+
+const clientFiles = [
+
+];
+const serverFiles = [
+
+];
+const files = [
+
+];
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use(packages);
+  api.imply(packages);
+  api.addFiles(clientFiles, 'client');
+  api.addFiles(serverFiles, 'server');
+  api.addFiles(files);
+});
